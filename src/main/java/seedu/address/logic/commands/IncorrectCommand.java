a789ea0b23f8f953839eea93b2f16bdee2c2be0f
nicholas lee <nicholaslwh97@gmail.com>
2019-03-11 21:17:49 +0800

    Added generateHash, logoutCommand and incorrectCommand

---
+++ src/main/java/seedu/address/logic/commands/IncorrectCommand.java
@@ -0,0 +1,24 @@
+package seedu.address.logic.commands;
+
+
+import seedu.address.logic.CommandHistory;
+import seedu.address.model.Model;
+
+/**
+ * Represents an incorrect command. Upon execution, produces some feedback to the user.
+ */
+public class IncorrectCommand extends Command{
+
+    public final String feedbackToUser;
+
+    public IncorrectCommand(String feedbackToUser){
+        this.feedbackToUser = feedbackToUser;
+    }
+
+    @Override
+    public CommandResult execute(Model model, CommandHistory history) {
+        return new CommandResult(feedbackToUser);
+    }
+
+
+}
