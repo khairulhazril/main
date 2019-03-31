9b8c62cf6a7df09b69c1db1343b01bcef715cfe2
Justin-Ong <justinong32@gmail.com>
2019-03-31 17:48:36 +0800

    Update tests

--- src/test/java/guitests/guihandles/CalendarPanelHandle.java
+++ src/test/java/guitests/guihandles/CalendarPanelHandle.java
@@ -1,24 +1,52 @@
 package guitests.guihandles;
 
-import guitests.GuiRobot;
+import static seedu.address.testutil.TypicalTasks.getTypicalTasks;
 
+import java.time.LocalDate;
+import java.time.YearMonth;
+
+import javafx.beans.value.ObservableBooleanValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.Node;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.GridPane;
+import seedu.address.model.task.Task;
+import seedu.address.ui.CalendarCell;
+import seedu.address.ui.CalendarPanel;
 
 /**
  * Provides a handle for {@code CalendarPanelHandle} containing the grid of
  * {@code CalendarContentCell}.
  */
-public class CalendarPanelHandle extends NodeHandle<Node> {
-    public static final String CALENDAR_PANEL_ID = "#calendarPanel";
+public class CalendarPanelHandle {
+    private static final ObservableList<Task> TYPICAL_TASKS =
+            FXCollections.observableList(getTypicalTasks());
 
-    public CalendarPanelHandle(Node calendarPanelNode) {
-        super(calendarPanelNode);
+    public static final String CALENDAR_PANEL_ID = "#calendarView";
+
+    private static final int COLS = 7; // 7 Days in a week
+    private static final int ROWS = 8; // 6 Rows + Day Header + Month Header
+
+    private static final YearMonth yearMonth = YearMonth.now();
+    private static LocalDate calendarDate = LocalDate.of(yearMonth.getYear(), yearMonth.getMonthValue(), 1);
+
+    private CalendarPanel calendarPanel;
+
+    public CalendarPanelHandle(CalendarPanel calendarPanel) {
+        this.calendarPanel = calendarPanel;
     }
 
     /**
      * Returns presence of calendar grid.
      */
-    public static boolean isWindowPresent() {
-        return new GuiRobot().isWindowShown(CALENDAR_PANEL_ID);
+    public boolean isWindowPresent() {
+        return (calendarPanel.getRoot() instanceof ScrollPane && calendarPanel.getTaskGridPane() != null);
     }
+
+    public boolean isGridPresent() {
+        return (calendarPanel.getTaskGridPane().getRowCount() == ROWS
+                && calendarPanel.getTaskGridPane().getColumnCount() == COLS);
+    }
+
 }
--- src/test/java/seedu/address/ui/CalendarPanelTest.java
+++ src/test/java/seedu/address/ui/CalendarPanelTest.java
@@ -1,4 +1,82 @@
 package seedu.address.ui;
 
-public class CalendarPanelTest {
+import static java.time.Duration.ofMillis;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
+import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.TypicalTasks.getTypicalTasks;
+
+import java.util.Collections;
+
+import org.junit.Test;
+
+import guitests.guihandles.CalendarPanelHandle;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import seedu.address.logic.Logic;
+import seedu.address.model.task.Due;
+import seedu.address.model.task.Module;
+import seedu.address.model.task.Name;
+import seedu.address.model.task.Priority;
+import seedu.address.model.task.Task;
+
+public class CalendarPanelTest extends GuiUnitTest {
+    private static final ObservableList<Task> TYPICAL_TASKS =
+            FXCollections.observableList(getTypicalTasks());
+
+    private static final long CARD_CREATION_AND_DELETION_TIMEOUT = 2500;
+
+    private final SimpleObjectProperty<Task> selectedTask = new SimpleObjectProperty<>();
+    private CalendarPanelHandle calendarPanelHandle;
+    private Logic logic;
+
+    /**
+     * Verifies that creating the calendarPanel works
+     */
+    @Test
+    public void initTest() {
+        initUi(createBackingList(1));
+        assertTrue(calendarPanelHandle.isWindowPresent());
+    }
+
+    @Test
+    public void gridTest() {
+        initUi(createBackingList(1));
+        assertTrue(calendarPanelHandle.isGridPresent());
+    }
+
+    @Test(timeout = 1000)
+    public void timingTest() {
+        initUi(createBackingList(1000));
+    }
+
+    /**
+     * Returns a list of persons containing {@code personCount} persons that is used to populate the
+     * {@code CalendarPanel}.
+     */
+    private ObservableList<Task> createBackingList(int personCount) {
+        ObservableList<Task> backingList = FXCollections.observableArrayList();
+        for (int i = 0; i < personCount; i++) {
+            Name name = new Name(i + "a");
+            Module module = new Module("CS2113");
+            Due due = new Due("01-03");
+            Priority priority = new Priority("1");
+            Task task = new Task(name, module, due, priority, Collections.emptySet());
+            backingList.add(task);
+        }
+        return backingList;
+    }
+
+    /**
+     * Initializes {@code personListPanelHandle} with a {@code TaskListPanel} backed by {@code backingList}.
+     * Also shows the {@code Stage} that displays only {@code TaskListPanel}.
+     */
+    private void initUi(ObservableList<Task> backingList) {
+        CalendarPanel calendarPanel =
+                new CalendarPanel(backingList, selectedTask, logic);
+        uiPartRule.setUiPart(calendarPanel);
+
+        calendarPanelHandle = new CalendarPanelHandle(calendarPanel);
+    }
 }
