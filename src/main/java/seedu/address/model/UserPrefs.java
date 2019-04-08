08425ad32637d6d64ff118a153f836170320f6a0
Justin-Ong <justinong32@gmail.com>
2019-04-08 23:45:35 +0800

    Rename taskmanager.json to StudentBuddy.json

--- src/main/java/seedu/address/model/UserPrefs.java
+++ src/main/java/seedu/address/model/UserPrefs.java
@@ -14,7 +14,7 @@ import seedu.address.commons.core.GuiSettings;
 public class UserPrefs implements ReadOnlyUserPrefs {
 
     private GuiSettings guiSettings = new GuiSettings();
-    private Path taskManagerFilePath = Paths.get("data", "taskmanager.json");
+    private Path taskManagerFilePath = Paths.get("data", "StudentBuddy.json");
 
     /**
      * Creates a {@code UserPrefs} with default values.
