8a8c273875dba7f56cc3d62d7fb7d9d9e8e3bd4d
khairulhazril <khairulhazril@gmail.com>
2019-04-10 20:55:41 +0800

    Update PPP

---
+++ src/test/java/seedu/address/logic/parser/FindModuleCommandParserTest.java
@@ -0,0 +1,35 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+import seedu.address.logic.commands.FindModuleCommand;
+import seedu.address.model.task.ModuleContainsKeywordsPredicate;
+import seedu.address.model.task.NameContainsKeywordsPredicate;
+
+public class FindModuleCommandParserTest {
+
+    private FindModuleCommandParser parser = new FindModuleCommandParser();
+
+    @Test
+    public void parse_emptyArg_throwsParseException() {
+        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindModuleCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_validArgs_returnsFindCommand() {
+        // no leading and trailing whitespaces
+        FindModuleCommand expectedFindModuleCommand =
+                new FindModuleCommand(new ModuleContainsKeywordsPredicate(Arrays.asList("CS2113T", "CS2101")));
+        assertParseSuccess(parser, "CS2113T CS2101", expectedFindModuleCommand);
+
+        // multiple whitespaces between keywords
+        assertParseSuccess(parser, " \n CS2113T \n \t CS2101  \t", expectedFindModuleCommand);
+    }
+
+}
